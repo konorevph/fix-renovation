3281390ba8a1d59f7886f14db71583d35ff98428
konorevph <regouserk@mail.ru>
2023-12-10 17:14:50 +0300

    File paths have been changed

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
 <body>
     <?php
         $header = new Header();
-        $header->addNavItem("contacts", "Контакты", "/#", "/img/icons/contacts.svg");
+        $header->addNavItem("contacts", "Контакты", "/#", "img/icons/contacts.svg");
         echo ($header);
     ?>
     <section id="main-screen">
@@ -38,7 +38,7 @@
         <h4>Готовый дизайн-проект, работы и материалы<br> за 3 месяца от 610 т.р.</h4>
         <?php echo new MainButton("launch-information-btn", "Узнайте о запуске первым"); ?>
         <p>Старт весной 2016</p>
-        <?php echo new IconLink("scroll-link", "/#extra-info-contaier", "Листайте вниз, <br> чтобы ничего не упустить", "/img/icons/down-arrows.svg") ?>
+        <?php echo new IconLink("scroll-link", "/#extra-info-contaier", "Листайте вниз, <br> чтобы ничего не упустить", "img/icons/down-arrows.svg") ?>
 
         <aside>
             <button class="play-btn">
@@ -59,9 +59,9 @@
     <?php  
         $slider = new ImgSlider_1();
         $slider->addHeader("3 стиля интерьера", "Скандинавский, классический и эко-стиль");
-        $slider->addItem("", "/img/interiors/slider_1.jpg", "/img/interiors/slider_1@2x.jpg", "/img/interiors/slider_1@3x.jpg", "Кафельная плитка<br>в ванной комнате<br>в скандинавском стиле");
-        $slider->addItem("", "/img/interiors/slider_2.jpg", "/img/interiors/slider_2@2x.jpg", "/img/interiors/slider_2@3x.jpg", "Кафельная плитка<br>в ванной комнате<br>в скандинавском стиле");
-        $slider->addItem("", "/img/interiors/slider_3.jpg", "/img/interiors/slider_3@2x.jpg", "/img/interiors/slider_3@3x.jpg", "Кафельная плитка<br>в ванной комнате<br>в скандинавском стиле");
+        $slider->addItem("", "img/interiors/slider_1.jpg", "img/interiors/slider_1@2x.jpg", "img/interiors/slider_1@3x.jpg", "Кафельная плитка<br>в ванной комнате<br>в скандинавском стиле");
+        $slider->addItem("", "img/interiors/slider_2.jpg", "img/interiors/slider_2@2x.jpg", "img/interiors/slider_2@3x.jpg", "Кафельная плитка<br>в ванной комнате<br>в скандинавском стиле");
+        $slider->addItem("", "img/interiors/slider_3.jpg", "img/interiors/slider_3@2x.jpg", "img/interiors/slider_3@3x.jpg", "Кафельная плитка<br>в ванной комнате<br>в скандинавском стиле");
         echo $slider;
     ?>
 
@@ -72,7 +72,7 @@
             $form->addInput("mail", "E-mail адрес*", "text", true);
             $form->addInput("tel", "Телефон", "text", false);
             $form->setBtnText("Подписаться");
-            $form->setSpetialOffer("Первым 10 клиентам стальная<br>входная дверь в подарок", "/img/icons/door.svg");
+            $form->setSpetialOffer("Первым 10 клиентам стальная<br>входная дверь в подарок", "img/icons/door.svg");
             $form->setSignature("* Обязательные для заполнения поля", "/#", "Политика конфиденциальности");
             echo $form;
         ?>
@@ -81,9 +81,9 @@
     <section id="">
         <?php
             $footer = new Footer();
-            $footer->addSocialNetwork("/img/icons/facebook.svg", "/#");
-            $footer->addSocialNetwork("/img/icons/vk.svg", "/#");
-            $footer->addSocialNetwork("/img/icons/instagram.svg", "/#");
+            $footer->addSocialNetwork("img/icons/facebook.svg", "/#");
+            $footer->addSocialNetwork("img/icons/vk.svg", "/#");
+            $footer->addSocialNetwork("img/icons/instagram.svg", "/#");
             $footer->setHeading("Следите за нашим развитием");
             $footer->setParagraph("Подпишитесь на наши аккаунты в соц. сетях, знакомьтесь<br>с нашими проектами и тем, что нас вдохновляет");
             $footer->setPhoneNumber('+7 (812) 903-54-45');
